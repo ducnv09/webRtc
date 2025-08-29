cb890f9fbac4b309702718bd96ed57afecfa6dc6
ducnv <ducdatcuong@gmail.com>
2025-08-29 17:00:28 +0700

    message, room resolver

---
+++ server/src/resolvers/message.resolver.ts
@@ -0,0 +1,54 @@
+import { Resolver, Mutation, Query, Args, Subscription } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { PubSub } from 'graphql-subscriptions';
+import { MessageService } from '../services/message.service';
+import { CreateMessageInput } from '../graphql/inputs/message.input';
+import { Message } from '../graphql/types/message.type';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { CurrentUser } from '../common/decorators/current-user.decorator';
+import { MessageSentEvent } from '../graphql/subscriptions/room.subscription';
+
+const pubSub = new PubSub();
+
+@Resolver(() => Message)
+export class MessageResolver {
+  constructor(private messageService: MessageService) {}
+
+  @Mutation(() => Message)
+  @UseGuards(JwtAuthGuard)
+  async sendMessage(
+    @Args('input') input: CreateMessageInput,
+    @CurrentUser() user: any,
+  ): Promise<Message> {
+    const message = await this.messageService.createMessage(input, user.userId);
+    pubSub.publish('messageSent', { 
+      messageSent: { roomId: input.roomId, message } 
+    });
+    return message;
+  }
+
+  @Query(() => [Message])
+  @UseGuards(JwtAuthGuard)
+  async roomMessages(
+    @Args('roomId') roomId: string,
+    @Args('limit', { defaultValue: 50 }) limit: number,
+    @Args('offset', { defaultValue: 0 }) offset: number,
+    @CurrentUser() user: any,
+  ): Promise<Message[]> {
+    return this.messageService.getRoomMessages(roomId, user.userId, limit, offset);
+  }
+
+  @Mutation(() => Boolean)
+  @UseGuards(JwtAuthGuard)
+  async deleteMessage(
+    @Args('messageId') messageId: string,
+    @CurrentUser() user: any,
+  ): Promise<boolean> {
+    return this.messageService.deleteMessage(messageId, user.userId);
+  }
+
+  @Subscription(() => MessageSentEvent)
+  messageSent(@Args('roomId') roomId: string) {
+    return pubSub.asyncIterator('messageSent');
+  }
+}
--- server/src/resolvers/room.resolver.ts
+++ server/src/resolvers/room.resolver.ts
@@ -15,18 +15,113 @@ import {
 
 const pubSub = new PubSub();
 
-@Resolver(() => Room) 
+@Resolver(() => Room)
 export class RoomResolver {
-    constructor(private roomService: RoomService) {}
+  constructor(private roomService: RoomService) {}
 
-    @Mutation(() => Room)
-    @UseGuards(JwtAuthGuard)
-    async createRoom(
-        @Args('input') input: CreateRoomInput,
-        @CurrentUser() user: any,
-    ): Promise<Room> {
-        const room = await this.roomService.create(input, user.userId);
-        pubSub.publish('roomCreated', { roomCreated: room });
-        return room;
+  @Mutation(() => Room)
+  @UseGuards(JwtAuthGuard)
+  async createRoom(
+    @Args('input') input: CreateRoomInput,
+    @CurrentUser() user: any,
+  ): Promise<Room> {
+    const room = await this.roomService.create(input, user.userId);
+    pubSub.publish('roomCreated', { roomCreated: room });
+    return room;
+  }
+
+  @Query(() => [Room])
+  @UseGuards(JwtAuthGuard)
+  async rooms(): Promise<Room[]> {
+    return this.roomService.findAll();
+  }
+
+  @Query(() => Room)
+  @UseGuards(JwtAuthGuard)
+  async room(@Args('id') id: string): Promise<Room> {
+    return this.roomService.findById(id);
+  }
+
+  @Mutation(() => Boolean)
+  @UseGuards(JwtAuthGuard)
+  async joinRoom(
+    @Args('roomId') roomId: string,
+    @CurrentUser() user: any,
+  ): Promise<boolean> {
+    const result = await this.roomService.joinRoom(roomId, user.userId);
+    
+    if (result) {
+      const userInfo = await this.roomService.findById(roomId);
+      pubSub.publish('userJoinedRoom', { 
+        userJoinedRoom: { roomId, user: userInfo } 
+      });
     }
+    
+    return result;
+  }
+
+  @Mutation(() => Boolean)
+  @UseGuards(JwtAuthGuard)
+  async leaveRoom(
+    @Args('roomId') roomId: string,
+    @CurrentUser() user: any,
+  ): Promise<boolean> {
+    const result = await this.roomService.leaveRoom(roomId, user.userId);
+    
+    if (result) {
+      pubSub.publish('userLeftRoom', { 
+        userLeftRoom: { roomId, userId: user.userId } 
+      });
+    }
+    
+    return result;
+  }
+
+  @Mutation(() => Room)
+  @UseGuards(JwtAuthGuard)
+  async updateRoom(
+    @Args('roomId') roomId: string,
+    @Args('input') input: UpdateRoomInput,
+    @CurrentUser() user: any,
+  ): Promise<Room> {
+    const room = await this.roomService.updateRoom(roomId, user.userId, input);
+    pubSub.publish('roomUpdated', { roomUpdated: room });
+    return room;
+  }
+
+  @Mutation(() => Boolean)
+  @UseGuards(JwtAuthGuard)
+  async deleteRoom(
+    @Args('roomId') roomId: string,
+    @CurrentUser() user: any,
+  ): Promise<boolean> {
+    return this.roomService.deleteRoom(roomId, user.userId);
+  }
+
+  @Query(() => [User])
+  @UseGuards(JwtAuthGuard)
+  async roomMembers(@Args('roomId') roomId: string): Promise<User[]> {
+    return this.roomService.getRoomMembers(roomId);
+  }
+
+  // Subscriptions
+  @Subscription(() => Room)
+  roomCreated() {
+    return pubSub.asyncIterator('roomCreated');
+  }
+
+  @Subscription(() => UserJoinedRoomEvent)
+  userJoinedRoom(@Args('roomId') roomId: string) {
+    return pubSub.asyncIterator('userJoinedRoom');
+  }
+
+  @Subscription(() => UserLeftRoomEvent)
+  userLeftRoom(@Args('roomId') roomId: string) {
+    return pubSub.asyncIterator('userLeftRoom');
+  }
+
+  @Subscription(() => RoomUpdatedEvent)
+  roomUpdated(@Args('roomId') roomId: string) {
+    return pubSub.asyncIterator('roomUpdated');
+  }
 }
